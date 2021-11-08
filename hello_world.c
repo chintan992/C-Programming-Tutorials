e0d51e9b28e7957b3eb2817c8ee94e808ae86e00
Chintan Rathod <chintanr21@gmail.com>
2021-11-08 05:07:09 +0000

    hello world

---
+++ hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //standard input output library
+
+//defining main function 
+void main()
+{ //starting paranthesis
+    //to print some text in c language we use "printf()"
+    printf("Hello! World");
+} //closing paranthesis
